dda323f7831cb9b109c06cfba5ab3df43b2d23b1
brian m. carlson <sandals@crustytoothpaste.net>
2014-03-08 01:12:16 +0000

    Make the debug generator support post-processing.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- processors/generators/debug.rb
+++ processors/generators/debug.rb
@@ -4,6 +4,10 @@ module Extruder
       def process(msg)
         puts [msg.digest_as_hex, msg.metadata].inspect
       end
+
+      def postprocess(msgs, results)
+        puts results.inspect
+      end
     end
   end
 end
