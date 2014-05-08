3d90fee0617644a8cc02fc7c6a5e584b68904f6a
brian m. carlson <sandals@crustytoothpaste.net>
2014-05-08 23:03:21 +0000

    Improve tests for netmask processor.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/netmask.rb
+++ spec/netmask.rb
@@ -5,7 +5,15 @@ $:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 $:.unshift File.join(File.dirname(__FILE__), '..', 'processors')
 
 require 'extruder'
+require 'extruder/mail'
 require 'processors/netmask'
+
+message = <<EOM
+From: alice@nonexistent.tld
+To: bob@nonexistent.tld
+
+Message
+EOM
 
 describe Extruder::Processor::NetmaskProcessor do
   it "has the correct type" do
@@ -20,5 +28,27 @@ describe Extruder::Processor::NetmaskProcessor do
   it "cannot process" do
     p = Extruder::Processor::NetmaskProcessor.new
     expect(p).not_to respond_to(:process)
+  end
+
+  it "can extract an IPv4 address from message metadata" do
+    ip = "192.168.2.1"
+    metadata = { originserver: { address: ip } }
+    m = Extruder::Message.new(message, metadata)
+    p = Extruder::Processor::NetmaskProcessor.new
+    ranges = (16..32).map { |mask| IPAddr.new(ip).mask(mask) }
+    expected = { address_ranges: {} }
+    ranges.each { |r| expected[:address_ranges][r] = [m.digest_as_hex] }
+    expect(p.postprocess([m], nil)).to eq expected
+  end
+
+  it "can extract an IPv4 address from message metadata" do
+    ip = "fe80::15"
+    metadata = { originserver: { address: ip } }
+    m = Extruder::Message.new(message, metadata)
+    p = Extruder::Processor::NetmaskProcessor.new
+    ranges = (48..128).map { |mask| IPAddr.new(ip).mask(mask) }
+    expected = { address_ranges: {} }
+    ranges.each { |r| expected[:address_ranges][r] = [m.digest_as_hex] }
+    expect(p.postprocess([m], nil)).to eq expected
   end
 end
