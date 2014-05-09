10daea38839624f9436fcf10d97a5ee8338c09b1
brian m. carlson <sandals@crustytoothpaste.net>
2014-05-09 23:15:38 +0000

    Add additional tests for the Postfix filter generator.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/postfix-filter.rb
+++ spec/postfix-filter.rb
@@ -7,6 +7,19 @@ $:.unshift File.join(File.dirname(__FILE__), '..', 'processors')
 require 'extruder'
 require 'generators/postfix-filter'
 
+module Extruder
+  module Generator
+    class PostfixFilterProcessor
+      attr_accessor :output
+
+      def puts(*args)
+        @output ||= []
+        @output << args
+      end
+    end
+  end
+end
+
 describe Extruder::Generator::PostfixFilterProcessor do
   it "has the correct type" do
     expect(Extruder::Generator::PostfixFilterProcessor.type).to eq :generator
@@ -67,5 +80,18 @@ describe Extruder::Generator::PostfixFilterProcessor do
     range_map[IPAddr.new("192.168.2.0/24")] = [1, 2, 3, 4, 5]
     result = Set.new([IPAddr.new("192.168.2.0/24")])
     expect(p.send(:compute_minimal_ranges, range_map)).to eq result
+  end
+
+  it "produces properly formatted output" do
+    ranges = ["192.168.2.1/32", "192.168.3.1/32", "192.168.4.128/25"]
+    metadata = {address_ranges: {}}
+    ranges.each { |r| metadata[:address_ranges][IPAddr.new(r)] = [1, 2, 3] }
+    reason = "junk"
+    p = Extruder::Generator::PostfixFilterProcessor.new({ "netmask_threshold" => 1, "reject_reason" => reason})
+    p.postprocess([], metadata)
+    expected = ranges.map do |r|
+      "#{r}\t\tREJECT #{reason} (#{r})"
+    end
+    expect(p.output.flatten).to eq expected
   end
 end
