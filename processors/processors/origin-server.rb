7035441d1eb10dccc4ad70b0af5e8fad59beb2bf
brian m. carlson <sandals@crustytoothpaste.net>
2014-06-15 21:07:21 +0000

    Document parts of origin-server.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- processors/processors/origin-server.rb
+++ processors/processors/origin-server.rb
@@ -1,10 +1,20 @@
 module Extruder
   module Processor
+
+    # Process the received headers to determine the origin server.
+    #
+    # The origin server is the first one that is not internal and does not match
+    # any of the exclude patterns.  A server is matched by its RDNS entry, or
+    # its IP address if no RDNS entry is present.
     class OriginServerProcessor
       def self.type
         :processor
       end
 
+      # Create a new processor.
+      #
+      # @param patterns [Enumerable] a set of regular expressions indicating
+      #   servers to ignore
       def initialize(patterns = [])
         @patterns = patterns.map { |pat| Regexp.compile(pat) }
       end
@@ -15,6 +25,7 @@ module Extruder
       end
 
       private
+
       def find_origin(hops)
         hops.each do |hop|
           hostname = hop[:rdns] || hop[:address]
